310458147e0a5d88d062802915ae4b553e365840
João Gabriel Bento <joaogabriel.bmm@gmail.com>
2024-06-19 18:09:04 -0300

    feat: create user entity

---
+++ src/main/java/com/icev/mobile/tea/domain/User.java
@@ -0,0 +1,40 @@
+package com.icev.mobile.tea.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String email;
+
+    private Date dateOfBirth;
+
+    private Boolean isASD;
+
+    private Boolean knowSomeoneWithASD;
+
+    private Boolean deleted;
+
+    private Date createdAt;
+
+    private Date updateAt;
+}
